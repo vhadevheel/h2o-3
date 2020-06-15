52066138f0c8a2efee8c0166fec3bf9d39ec0082
zuzanaolajcova <zuzana.olajcova@h2o.ai>
2020-06-15 13:07:27 +0200

    PUBDEV-7237 condition fix

--- h2o-core/src/main/java/hex/ModelBuilder.java
+++ h2o-core/src/main/java/hex/ModelBuilder.java
@@ -1212,7 +1212,7 @@ abstract public class ModelBuilder<M extends Model<M,P,O>, P extends Model.Param
       hide("_keep_cross_validation_predictions", "Only for cross-validation.");
       hide("_keep_cross_validation_fold_assignment", "Only for cross-validation.");
       hide("_fold_assignment", "Only for cross-validation.");
-      if (_parms._fold_assignment != Model.Parameters.FoldAssignmentScheme.AUTO & _parms._fold_assignment != null) {
+      if (_parms._fold_assignment != Model.Parameters.FoldAssignmentScheme.AUTO && _parms._fold_assignment != null) {
         error("_fold_assignment", "Fold assignment is only allowed for cross-validation.");
       }
     }
